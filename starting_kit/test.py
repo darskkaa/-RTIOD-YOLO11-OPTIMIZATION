78243729555de56c9ad5eaabccdd5ca67427e993
MarcoParola <marcoparola96@gmail.com>
2025-10-16 16:26:19 +0200

    parametrize checkpoint path to load model

--- starting_kit/test.py
+++ starting_kit/test.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 from ultralytics.utils.plotting import plot_results
 
-model = YOLO("checkpoints/best.pt")
+model = YOLO(args.modelCheckpoint)
 
 if __name__ == "__main__":
     res = model.val(data="data/data.yaml", save_json=True, plots=True)
