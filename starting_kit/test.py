3de4cb81cbcb2f8530cb35dee867e30bfe042483
MarcoParola <marcoparola96@gmail.com>
2025-10-20 10:53:35 +0200

    fix model name to be imported and put it into the config file

--- starting_kit/test.py
+++ starting_kit/test.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 from ultralytics.utils.plotting import plot_results
+import hydra
 
-model = YOLO(args.modelCheckpoint)
+@hydra.main(config_path='config', config_name='config', version_base="1.3")
+def main(args):
+    model = YOLO(args.modelCheckpoint)
+    res = model.val(data="data/data.yaml", save_json=True, plots=True)
 
 if __name__ == "__main__":
-    res = model.val(data="data/data.yaml", save_json=True, plots=True)
+    main()
--- starting_kit/train.py
+++ starting_kit/train.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 from ultralytics.utils.plotting import plot_results
+import hydra
 
-model = YOLO("yolov8s.pt")
+@hydra.main(config_path='config', config_name='config', version_base="1.3")
+def main(args):
+    model = YOLO(args.modelCheckpoint)
+    model.train(data="data/data.yaml", epochs=20, imgsz=160, batch=64, lr0=0.0001, close_mosaic=0)
+    res = model.val(data="data/data.yaml", save_json=True, plots=True)
 
 if __name__ == "__main__":
-    model.train(data="data/data.yaml", epochs=25, imgsz=160, batch=64, lr0=0.0005)
-    res = model.val(data="data/data.yaml", save_json=True, plots=True)
-    #plot_results("runs/detect/exp/results.csv", dir="runs/detect/exp")
+    main()
