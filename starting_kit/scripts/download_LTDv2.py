fe37bb3117deea9297de565e04577d62b162b513
Marco Parola <32603898+MarcoParola@users.noreply.github.com>
2025-10-29 10:10:41 +0100

    Add download for TestNoLabels.json dataset

--- starting_kit/scripts/download_LTDv2.py
+++ starting_kit/scripts/download_LTDv2.py
@@ -5,6 +5,7 @@ def download_LTDv2():
     # download the LTDv2 dataset annotations from huggingface
     hf_hub_download(repo_id="vapaau/LTDv2", repo_type="dataset", filename="data/Valid.json")
     hf_hub_download(repo_id="vapaau/LTDv2", repo_type="dataset", filename="data/Train.json")
+    hf_hub_download(repo_id="vapaau/LTDv2", repo_type="dataset", filename="data/TestNoLabels.json")
     hf_hub_download(repo_id="vapaau/LTDv2", repo_type="dataset", filename="data/frames.zip")
 
 if __name__ == "__main__":
